9b58d9b8024d078930a8d55988de237e40ba60da
Hugo Bessa <hugobessaa@gmail.com>
2015-08-14 16:18:21 -0300

    Add more complete component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-export function add(x, y) {
-  return x + y;
-}
+import React, { PropTypes } from 'react';
+
+const Placeholdit = React.createClass({
+  propTypes: {
+    width: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ]).isRequired,
+    height: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ]),
+    format: PropTypes.oneOf(['gif', 'png', 'jpg', 'jpeg']),
+    children: PropTypes.func
+  },
+
+  getDefaultProps() {
+    return {
+      format: 'gif'
+    };
+  },
+
+  render() {
+    const width = this.props.width;
+    const height = this.props.height || this.props.width;
+    const format = this.props.format;
+
+    const src = `https://placehold.it/${width}x${height}.${format}`;
+
+    return (
+      <img 
+        src={src}
+        width={width} 
+        height={height} />
+    );
+  }
+});
+
+export default Placeholdit;
---
+++ test/components/placeholdit.js
@@ -0,0 +1,67 @@
+import expect from 'expect';
+import React from 'react/addons';
+import Placeholdit from '../../src';
+
+const { TestUtils } = React.addons;
+const shallowRenderer = TestUtils.createRenderer();
+
+function shallowComponent(component) {
+  const shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(component);
+  return shallowRenderer.getRenderOutput();
+}
+
+describe('Placeholdit', function() {
+  describe('Basic usage', () => {
+    const simpleComponent = shallowComponent(
+      <Placeholdit width="200" />
+    );
+
+    const bothComponent = shallowComponent(
+      <Placeholdit width="200" height="100" />
+    );
+
+    it('should display an img tag', () => {
+      expect(simpleComponent.type).toBe('img');
+      expect(bothComponent.type).toBe('img');
+    });
+
+    it('should display an img tag with the right dimensions', () => {
+      expect(simpleComponent.props.width).toBe('200');
+      expect(simpleComponent.props.height).toBe('200');
+
+      expect(bothComponent.props.width).toBe('200');
+      expect(bothComponent.props.height).toBe('100');
+    });
+
+    it('should display an img with the right src', () => {
+      expect(simpleComponent.props.src).toBe('https://placehold.it/200x200.gif');
+      expect(bothComponent.props.src).toBe('https://placehold.it/200x100.gif');
+    });
+  });
+
+  describe('Format', () => {
+    const defaultComponent = shallowComponent(
+      <Placeholdit width="300" height="300" />
+    );
+
+    const pngComponent = shallowComponent(
+      <Placeholdit width="300" height="300" format="png" />
+    );
+
+    const jpgComponent = shallowComponent(
+      <Placeholdit width="300" height="300" format="jpg" />
+    );
+
+    const jpegComponent = shallowComponent(
+      <Placeholdit width="300" height="300" format="jpeg" />
+    );
+
+    it('should display an img with the correct format', () => {
+      expect(defaultComponent.props.src).toBe('https://placehold.it/300x300.gif');
+      expect(pngComponent.props.src).toBe('https://placehold.it/300x300.png');
+      expect(jpgComponent.props.src).toBe('https://placehold.it/300x300.jpg');
+      expect(jpegComponent.props.src).toBe('https://placehold.it/300x300.jpeg');
+    });
+  });
+});
