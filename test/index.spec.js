9a764e2ded94caeaf16a363ecf7fc80f4e50fc92
Hugo Bessa <hugobessaa@gmail.com>
2015-08-14 14:37:27 -0300

    Add simple tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,29 @@
 import expect from 'expect';
-import { add } from '../src';
+import jsdomReact from './jsdomReact';
+import React from 'react/addons';
+import Placeholdit from '../src';
 
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+const { TestUtils } = React.addons;
+
+describe('Placeholdit', () => {
+  jsdomReact();
+
+
+  it('should display an img tag with the right dimensions', () => {
+    const tree = TestUtils.renderIntoDocument(
+      <Placeholdit width="200" height="200" />
+    );
+
+    const imgComponent = TestUtils.findRenderedDOMComponentWithTag(
+      tree,
+      'img'
+    );
+
+    const imgElement = imgComponent.getDOMNode();
+
+    expect(imgElement).toExist();
+    expect(imgElement.tagName).toBe('img');
+    expect(imgElement.getAttribute('width')).toBe('200');
+    expect(imgElement.getAttribute('height')).toBe('200');
   });
 });
